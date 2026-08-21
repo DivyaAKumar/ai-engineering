2eb9afba66c6a31d755b6c38d3143ed76eb47a80
Divya A Kumar <165883358+DivyaAKumar@users.noreply.github.com>
2026-08-21 16:23:08 +0530

    code quality formatting

--- hello.py
+++ hello.py
@@ -1,14 +1,14 @@
 import requests
 
 # We need coordinates to get weather data
-latitude = 48.85   # Paris latitude
-longitude = 2.35   # Paris longitude
+latitude = 48.85  # Paris latitude
+longitude = 2.35  # Paris longitude
 
 # Build the API URL with our parameters
 url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m"
 
 # Make the request
-response = requests.get(url,timeout=10)
+response = requests.get(url, timeout=10)
 data = response.json()
 
 print(data)
@@ -20,9 +20,9 @@ import os
 load_dotenv(r"D:\pythonProj\python-for-ai\sales-analysis\data\.env")
 
 # Now use your variables
-api_key = os.environ.get('API_KEY')
-debug = os.environ.get('DEBUG')
-database = os.environ.get('DATABASE_URL')
+api_key = os.environ.get("API_KEY")
+debug = os.environ.get("DEBUG")
+database = os.environ.get("DATABASE_URL")
 print(f"API Key: {api_key}")
 print(f"Debug mode: {debug}")
 print(f"Database URL: {database}")
