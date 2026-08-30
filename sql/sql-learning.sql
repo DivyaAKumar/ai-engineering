352dfe49862d908eb7f384c965d5bc3a4c7e3f0f
Divya A Kumar <165883358+DivyaAKumar@users.noreply.github.com>
2026-08-30 19:52:17 +0530

    Create Table and insert values in sql

---
+++ sql/sql-learning.sql
@@ -0,0 +1,40 @@
+--USE SQLTutorial;
+--CREATE TABLE EmployeeDemographics
+--(EmpID int,
+--FirstName varchar(50),
+--LastName varchar(50),
+--Age int,
+--Gender varchar(50)
+--)
+
+--CREATE TABLE EmployeeSalary
+--(EmployeeID int,
+--JobTitle varchar(50),
+--Salrary int)
+
+Insert into EmployeeDemographics VALUES
+(1001, 'Div', 'Kumar', 21, 'F'),
+(1002, 'Nav', 'Chandrakar', 23, 'M'),
+(1003, 'Harsh', 'Tiwari', 26, 'M'),
+(1004, 'Ayushi', 'Dubey', 22, 'F'),
+(1005, 'Sana', 'Uke', 25, 'F'),
+(1006, 'Nandini', 'Rishi', 23, 'F'),
+(1007, 'Harshit', 'Kumar', 27, 'M'),
+(1008, 'Parth', 'Sharma', 29, 'M'),
+(1009, 'Surya', 'Manasa', 22, 'F'),
+(1010, 'Shreya', 'Panigrahi', 20, 'F'),
+(1011, 'Mim', 'Sharma', 21, 'F')
+
+INSERT INTO EmployeeSalary VALUES
+(1001, 'Software Engineer', 55000),
+(1002, 'Data Analyst', 48000),
+(1003, 'Project Manager', 75000),
+(1004, 'HR Manager', 60000),
+(1005, 'Software Engineer', 58000),
+(1006, 'Data Scientist', 70000),
+(1007, 'Accountant', 50000),
+(1008, 'Sales Executive', 45000),
+(1009, 'Marketing Manager', 65000),
+(1010, 'Business Analyst', 52000),
+(1011, 'Software Engineer', 56000);
+
